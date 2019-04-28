0a045065ffdb86c7e807263c7a504b1cc1e73bf0
jacobhanson <jhanreg11@gmail.com>
2019-04-27 19:50:57 -0700

    doing some wrk

--- CashOut/controllers.py
+++ CashOut/controllers.py
@@ -1,7 +1,16 @@
 from CashOut import app, db
 from flask import render_template, redirect, url_for
-
+from CashOut.models import Offer
 
 @app.route('/')
 def home():
     return "HEllo WOrld"
+
+
+@app.route('/API/v1.0/get_offers', methods=['GET'])
+def get_offers():
+    offers = Offer.query.all()
+    jsonList = []
+    for x in offers:
+        jsonList.append({'id': x.id, 'cash_amt': x.cash_amt})
+    return jsonList
