e0ce627fa49f0c7ded50b494f78746b95d570525
jacobhanson <jhanreg11@gmail.com>
2019-04-27 22:16:08 -0700

    "doing som wrk"

--- CashOut/controllers.py
+++ CashOut/controllers.py
@@ -1,16 +1,30 @@
 from CashOut import app, db
-from flask import render_template, redirect, url_for
-from CashOut.models import Offer
+from flask import request, jsonify
+from CashOut.models import *
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return "HEllo WOrld"
 
-
-@app.route('/API/v1.0/get_offers', methods=['GET'])
-def get_offers():
+@app.route('/api/offer', methods=['GET'])
+def get_offer():
+    long = request.args.get('long')
+    lat = request.args.get('long')
+    range = request.args.get('range')
+    list = []
     offers = Offer.query.all()
-    jsonList = []
     for x in offers:
-        jsonList.append({'id': x.id, 'cash_amt': x.cash_amt})
-    return jsonList
+
+        list.append(x.to_json())
+    return jsonify({'offers': list})
+
+@app.route('/api/offer', methods=['POST'])
+def post_offer():
+    data = request.get_json(force=True)
+    print (data)
+    newOffer = Offer.add_offer(data['cash_amt'], data['venmo_amt'], data['long'], data['lat'], data['user_id'])
+    if newOffer:
+        return jsonify({'success': True})
+    else:
+        return jsonify({'success': False})
+
--- CashOut/models.py
+++ CashOut/models.py
@@ -5,15 +5,56 @@ class User(db.Model):
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     venmo_name = db.Column(db.String(20), nullable=False)
+    offers = db.relationship('Offer', backref='user', lazy=True)
+
+    def add_user(username, password, venmo_name):
+        new_user = User(username=username, password='default_password', venmo_name=venmo_name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    def get_json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'venmo_name': self.venmo_name,
+            'offers': [offer.id for offer in self.offers]
+        }
 
 class Offer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    Cash_amt = db.Column(db.Float(precision=2, asdecimal=True), nullable=False, default=0)
-    venmo_amt = db.Column(db.Float(precision=2, asdecimal=True), nullable=False, default=0)
-    type = db.Column(db.Boolean, nullable=False)
-    long = db.Column(db.Float(precision=4, asdecimal=True), nullable=False)
-    lat = db.Column(db.Float(precision=4, asdecimal=True), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # user through backref
 
-class UserToOffer(db.Model):
-    user_id = db.Column(db.Integer(nullable=False))
-    offer_id = db.Column(db.Integer(nullable=False))
+    ### Amounts ###
+    cash_amt = db.Column(db.Float(precision=2, asdecimal=True), nullable=False, default=0)
+    venmo_amt = db.Column(db.Float(precision=2, asdecimal=True), nullable=False, default=0)
+
+    ### Offer state ###
+    # if true offer hasn't been accepted by anyone else
+    is_completed = db.Column(db.Boolean, default=False, nullable=False)
+
+    ### Location ###
+    long = db.Column(db.Float(precision=4, asdecimal=True))
+    lat = db.Column(db.Float(precision=4, asdecimal=True))
+
+    def add_offer(cash_amt, venmo_amt, long, lat, user_id):
+        new_offer = Offer(cash_amt=cash_amt, venmo_amt=venmo_amt, is_completed=False, long=long, lat=lat, user_id=user_id)
+        db.session.add(new_offer)
+        db.session.commit()
+        return new_offer
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'user_id': self.user.id,
+            'cash_amt': self.cash_amt,
+            'venmo_amt': self.venmo_amt,
+            'is_completed': self.is_completed,
+            'location': {
+                'lat': self.lat,
+                'long': self.long
+            }
+        }
+
