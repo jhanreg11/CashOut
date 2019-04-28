56cdd2c4f09806710c57eab5201b593a5e004b99
jacobhanson <jhanreg11@gmail.com>
2019-04-27 19:32:29 -0700

    started offer table

--- CashOut/models.py
+++ CashOut/models.py
@@ -1,8 +1,13 @@
 from CashOut import db
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     venmo_name = db.Column(db.String(20), nullable=False)
-    
+
+class Offer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Cash_amt = db.Column(db.Float(precision=2, asdecimal=True), nullable=False, default=0)
+    venmo_amt = db.Column(db.Float(precision=2, asdecimal=True), nullable=False, default=0)
+    type = db.Column(db.Boolean)
