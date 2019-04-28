87dbe4358cc75b6ce3112c2dfa30639ead1d506b
jacobhanson <jhanreg11@gmail.com>
2019-04-28 02:50:05 -0700

    styling index and submit

---
+++ CashOutClient/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#giver').click(function() {
+        window.location.replace('/submit.html?giving=true')
+    })
+
+    $('#taker').click(function() {
+        window.location.replace('/submit.html?giving=false')
+    })
+})
