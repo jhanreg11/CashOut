d3bbc9df9f2d5e14d4728fbf4188b2bfde021454
root <root@ip-172-31-10-184.us-west-1.compute.internal>
2019-04-28 10:28:36 +0000

    added by relative path

--- CashOutClient/js/index.js
+++ CashOutClient/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('#giver').click(function() {
-        window.location.replace('/submit.html?giving=true')
+        window.location.replace('./submit.html?giving=true')
     })
 
     $('#taker').click(function() {
-        window.location.replace('/submit.html?giving=false')
+        window.location.replace('./submit.html?giving=false')
     })
 })
